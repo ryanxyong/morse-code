e86fde0598bf4fcec68516138c061da6c8a7b2ef
ryanxyong <ryan.yong.24@dartmouth.edu>
2024-08-07 21:28:39 -0500

    init: inital files

---
+++ src/text-to-morse.c
@@ -0,0 +1,11 @@
+/*
+ * @file src/text-to-morse.c
+ *
+ * Represents a string as a series of periods and underscores like Morse code
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
