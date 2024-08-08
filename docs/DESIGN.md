2ff3b6b931bd49e1490b962971f555337195dbcd
ryanxyong <ryan.yong.24@dartmouth.edu>
2024-08-07 22:33:51 -0500

    feat: string conversion to morse code successful

---
+++ docs/DESIGN.md
@@ -0,0 +1,18 @@
+# Design Specifications
+### Created August 7, 2024
+### Version 1.0
+
+## Purpose
+* Take in words as a string and return a Morse code version as a string composed
+of periods and underscores.
+
+## Technologies
+* C
+
+## Functions
+* mapCharToMorse
+    - (Parameter) c: character used to find Morse code equivalent
+    - Converts character to an index and refers to bucket sorted arrays
+
+* main
+    - (Parameter) s: string to convert to Morse code
--- src/text-to-morse.c
+++ src/text-to-morse.c
@@ -7,5 +7,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
+const char* ALPHA_TO_MORSE[] = {
+    "._", "_...", "_._.", "_..", ".",
+    ".._.", "__.", "....", "..", ".___",
+    "_._", "._..", "__", "_.", "___",
+    ".__.", "__._", "._.", "...", "_",
+    ".._", "..._", ".__", "_.._", "_.__",
+    "__.."
+};
 
+const char* NUM_TO_MORSE[] = {
+    "_____", ".____", "..___", "...__", "...._",
+    ".....", "_....", "__...", "___..", "____."
+};
+
+const char* mapCharToMorse(char c) {
+    if (isalnum(c)) {
+        if (isalpha(c)) {
+            c = toupper(c);
+            int ascii = (int)c;
+            return ALPHA_TO_MORSE[ascii-65];
+        } else {
+            int ascii = (int)c;
+            return NUM_TO_MORSE[ascii-48];
+        }
+    } else if (c == ' ') {
+        return " ";
+    } else {
+        return "";
+    }
+}
+
+int main() {
+    printf("Welcome to the string to Morse code converter\n");
+    printf("Input a string to Morsify: ");
+    char input[256];
+    fgets(input, 256, stdin);
+
+    char morse[strlen(input)];
+    for (int i = 0; i < strlen(input); i++) {
+        const char* convert = mapCharToMorse(input[i]);
+        strcat(morse, convert);
+    }
+
+    printf("Here is the Morse code: %s\n", morse);
+    return 0;
+}
